cb766051c4d31861a05860ff930ecba51c060d0e
felipe.silva <felipe.silva@ictsirio.com>
2020-08-01 13:14:31 -0300

    organizacao de codigo

--- desafio-java/src/desafio/TestOne.java
+++ desafio-java/src/desafio/TestOne.java
@@ -12,19 +12,23 @@ public class TestOne {
 
 	public static void main(String[] args) {
 
+		executeTestOne();
+
+	}
+
+	private static void executeTestOne() {
 		int[] missingNumbers = getMissingNumbers();
 
 		for (int i = 0; i < missingNumbers.length; i++) {
 			System.out.println(missingNumbers[i]);
 		}
-
 	}
 
 	/**
 	 * 
 	 * @return int[]
 	 */
-
+	
 	private static int[] getMissingNumbers() {
 		int count = 0, min = 1, max = 50;
 		final int[] arr = { 32, 47, 41, 7, 47, 9, 36, 32, 20, 15, 11, 9, 6, 37, 9, 39, 45, 29, 29, 19, 28, 21, 50, 19, 43 };
